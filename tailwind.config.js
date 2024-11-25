ec64853b77e275af68827cd221639cba4fec6a62
Jaeho Cha <ischar@naver.com>
2024-11-25 15:16:37 +0900

    chore: add custom colors to Tailwind CSS
    - tailwind css에 커스텀 테마 설정

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        light: {
+          primary: "#ebedef",
+          card: "#ffffff",
+          primaryText: "#343434",
+          menuText: "#8e8e93",
+          subText: "#3f3f46",
+          activeMenu: "#ffb4b0",
+          colorCard: "#f6bdbc",
+          border: "#746d74",
+          input: "#f5f5f5",
+        },
+        dark: {
+          primary: "#1e1e1e",
+          card: "#2c2c2e",
+          primaryText: "#e4e4e7",
+          menuText: "#a1a1aa",
+          subText: "#d4d4d8",
+          activeMenu: "#ffb4b0",
+          colorCard: "#663f3f",
+          border: "#4a4a4a",
+          input: "#3A3A3C",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
